e26d59bb379a4c95c70ea9d2d5c8e50e327f0290
Sergiu Dumbrava <sergiu.dumbrava@gmail.com>
2015-05-02 16:16:03 -0500

    initial commit

---
+++ sdcd001/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sdcd001.Startup))]
+namespace sdcd001
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
